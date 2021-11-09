9ee9a6b1be134229d1c3254f62c2d86125c8804c
koji <koji4321@naver.com>
2021-11-09 20:35:52 +0900

    20211109-redis test

--- src/test/java/com/example/algostudy/AlgostudyApplicationTests.java
+++ src/test/java/com/example/algostudy/AlgostudyApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.algostudy;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class AlgostudyApplicationTests {
 
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void redisTest() {
+        ValueOperations<String, String> stringStringValueOperations = redisTemplate.opsForValue();
+        stringStringValueOperations.set("1", "redisTest");
+        String s = stringStringValueOperations.get("1");
+
     }
 
 }
